ed22f9e4a904fbceb55e85bfd2353935ba320b94
Martin <martin_vga4@hotmail.com>
2022-04-27 12:40:56 +0200

    implementado endpoint que borra el usuario y sus tablas asociadas

---
+++ 2-controllers/users/deleteUser.js
@@ -0,0 +1,121 @@
+const { getConnection } = require("../../1-db/db");
+const { deleteUpload } = require("../../helpers");
+
+async function deleteUser(req, res, next) {
+  let connection;
+  try {
+    connection = await getConnection();
+
+    const { id } = req.params;
+
+    // Compruebo que existe el usuario
+    const [user] = await connection.query(
+      `
+      SELECT id, image
+      FROM users
+      WHERE id=?
+    `,
+      [id]
+    );
+
+    if (user.length === 0) {
+      const error = new Error(
+        `No existe ningún usuario con id ${id} en la base de datos`
+      );
+      error.httpStatus = 404;
+      throw error;
+    }
+
+    //Comprobamos si el usuario tiene posts relacionados
+    const [post] = await connection.query(
+      `
+        SELECT id
+        FROM posts
+        WHERE users_id=?
+      `,
+      [id]
+    );
+
+    console.log(post);
+
+    if (post.length !== 0) {
+      console.log("Hay posts relacionados");
+
+      //Borramos las imagenes relacionadas al post
+      for (let index = 0; index < post.length; index++) {
+        console.log(post[index].id);
+        await connection.query(
+          `
+            DELETE
+            FROM images
+            WHERE post_id=?
+          `,
+          [post[index].id]
+        );
+      }
+      //Borramos los comments asociados al post.
+      for (let index = 0; index < post.length; index++) {
+        console.log(post[index].id);
+        await connection.query(
+          `
+            DELETE
+            FROM comments
+            WHERE post_id=?
+          `,
+          [post[index].id]
+        );
+      }
+
+      //Borramos los likes asociados al post.
+      for (let index = 0; index < post.length; index++) {
+        console.log(post[index].id);
+        await connection.query(
+          `
+            DELETE
+            FROM likes
+            WHERE post_id=?
+          `,
+          [post[index].id]
+        );
+      }
+
+      //Borramos los posts asociados al user.
+      for (let index = 0; index < post.length; index++) {
+        console.log(post[index].id);
+        await connection.query(
+          `
+            DELETE
+            FROM posts
+            WHERE id=?
+          `,
+          [post[index].id]
+        );
+      }
+    } else {
+      console.log("no hay post relacionados al usuario");
+    }
+
+    await connection.query(
+      `
+      DELETE FROM users
+      WHERE id=?
+    `,
+      [id]
+    );
+
+    if (user[0].image) {
+      await deleteUpload(user[0].image);
+    }
+
+    res.send({
+      status: "ok",
+      message: `El usuario con id ${id} fue borrado`,
+    });
+  } catch (error) {
+    next(error);
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
+module.exports = deleteUser;
--- helpers.js
+++ helpers.js
@@ -1,5 +1,6 @@
 const { format } = require("date-fns");
 const crypto = require("crypto");
+const fs = require("fs").promises;
 
 function formatDateToDB(date) {
   return format(new Date(date), "yyyy-MM-dd HH:mm:ss");
@@ -9,10 +10,14 @@ function randomString(length = 20) {
   return crypto.randomBytes(length).toString("hex").slice(0, length);
 }
 
+async function deleteUpload(uploadedImage) {
+  await fs.unlink(path.join(imageUploadPath, uploadedImage));
+}
+
 function generateError(message, code = 500) {
   const error = new Error(message);
   error.httpStatus = code;
   return error;
 }
 
-module.exports = { formatDateToDB, generateError, randomString };
+module.exports = { formatDateToDB, generateError, randomString, deleteUpload };
