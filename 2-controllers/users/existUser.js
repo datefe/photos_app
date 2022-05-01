63b1103374d8f6e51795f6fe881dce9502b4977f
Martin <martin_vga4@hotmail.com>
2022-05-01 18:15:39 +0200

    modificaciones getUser

---
+++ 2-controllers/users/existUser.js
@@ -0,0 +1,31 @@
+const { getConnection } = require("../../1-db/db");
+const { generateError } = require("../../helpers");
+
+const existUser = async (userName) => {
+  let connection;
+  try {
+    connection = await getConnection();
+
+    const [exist] = await connection.query(
+      `
+        SELECT id, name, surname, email, image, intro
+        FROM users
+        WHERE userName = ? ;
+        `,
+      [userName]
+    );
+
+    if (exist.length < 1) {
+      throw generateError(
+        `No hay ningun usuario con el userName:${userName}`,
+        404
+      );
+    }
+
+    return exist;
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = existUser;
---
+++ 2-controllers/users/postImages.js
@@ -0,0 +1,27 @@
+const { getConnection } = require("../../1-db/db");
+
+const postImage = async (id) => {
+  let connection;
+  try {
+    connection = await getConnection();
+
+    const [list] = await connection.query(
+      `
+    SELECT id, path
+    FROM images 
+    WHERE post_id = ?;
+    `,
+      [id]
+    );
+
+    if (list.length < 1) {
+      return "no hay imagenes ligadas al post";
+    }
+
+    return list;
+  } finally {
+    if (connection) connection.release();
+  }
+};
+
+module.exports = postImage;
